443dc0d3ba1313fba26e38bc36086c6f53bd355c
JoeDevelopment <admin@joedevelopment.net76.net>
2014-10-01 16:23:48 -0500

    Added main server constructor

--- src/joehttp2/JoeHTTP2.java
+++ src/joehttp2/JoeHTTP2.java
@@ -1,11 +1,24 @@
 package joehttp2;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author aweso_000
  */
 public class JoeHTTP2 {
 
+    private ServerSocket s = null;
     
+    public JoeHTTP2(int port) {
+        try {
+            s = new ServerSocket(port);
+        } catch (IOException ex) {
+            Logger.getLogger(JoeHTTP2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
